37c2dca12e16f66d7b8cb2ee0ec7ea4cdeec8c47
Michael <144923189+md4096@users.noreply.github.com>
2026-07-01 09:25:01 +0200

    Add attestation debug logging (#10)
    * Add attestation debug logging
    
    * Update test-jvm.yml

--- server/src/main/kotlin/at/asitplus/walletprovider/service/crypto/AttestationService.kt
+++ server/src/main/kotlin/at/asitplus/walletprovider/service/crypto/AttestationService.kt
@@ -28,6 +28,8 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.encodeToJsonElement
+import kotlin.random.Random
+import kotlin.random.nextUInt
 import kotlin.time.Clock.System.now
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.days
@@ -84,7 +86,7 @@ interface AttestationService {
                 }
 
                 is AttestationResponse.Failure -> {
-                    throw Throwable("Verifying key attested keys failed with: Type: ${response.kind}, Explanation: ${response.explanation}")
+                    throw Throwable("AttestationResponse.Failure: Type: ${response.kind}, Explanation: ${response.explanation}")
                 }
             }
         }
@@ -190,7 +192,19 @@ class RealAttestationService(
     @OptIn(ExperimentalTime::class)
     override suspend fun verifyKeyAttestedKeys(csr: Pkcs10CertificationRequest): AttestationResponse =
         attestationValidator.verifyAttestation(
-            csr
+            csr = csr,
+            onPreAttestationError = {
+                "onPreAttestationError: type: ${this.javaClass.simpleName}, errorId: ${createErrorId()}}".let {
+                    Napier.w("$it, message: ${this.throwable}, $csr")
+                    it
+                }
+            },
+            onAttestationError = { debugStatement ->
+                "onAttestationError: type: ${this.javaClass.simpleName}, errorId: ${createErrorId()}".let {
+                    Napier.w("$it, message: ${debugStatement.serializeCompact()}, $csr", cause)
+                    it
+                }
+            }
         ) { listOf() }
 
     override suspend fun issueChallenge() = Json.encodeToString(
@@ -200,3 +214,5 @@ class RealAttestationService(
         )
     )
 }
+
+fun createErrorId() = Random.nextUInt().toString(radix = 36)
